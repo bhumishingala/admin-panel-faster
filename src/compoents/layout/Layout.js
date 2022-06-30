7baeedcffe3842051b416baf8b0f3d6a8f4c1cfe
bhumishingala <shingalabhumi28@gmail.com>
2022-06-30 10:10:09 +0530

    drawer

---
+++ src/compoents/layout/Layout.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { NavLink } from 'react-router-dom';
+
+const drawerWidth = 240;
+
+export default function Layout({ childern }) {
+
+  const data = [
+    { lable: 'product', icon: <AddShoppingCartIcon />, to: "/products" }
+  ]
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Permanent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {data.map((text, index) => (
+            <ListItem component={NavLink} exact to={text.to} key={text.lable} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {text.icon}
+                </ListItemIcon>
+                {text.lable}
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        {childern}
+      </Box>
+    </Box>
+  );
+}
